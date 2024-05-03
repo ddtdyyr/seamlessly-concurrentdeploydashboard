d5534f1fe89fdcccefec90197f6d40f2575e3496
ddtdyyr <bsjcirkettlewis@hotmail.com>
2024-05-03 07:36:45 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const initializeArrayWithRange = (end, start = 0) =>
-  Array.from({ length: end - start }, (_, i) => i + start);
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
+  }
+  return maxProduct;
+}
