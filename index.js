865479242d81780c0f3d9a328d80716415c48d1c
ddtdyyr <bsjcirkettlewis@hotmail.com>
2024-05-09 08:48:25 +0700

    demo task

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function squareDigits(num) {
-  return parseInt(
-    num
-      .toString()
-      .split("")
-      .map((digit) => digit ** 2)
-      .join(""),
-  );
+function allPathsSourceTarget(graph) {
+  const result = [];
+  const target = graph.length - 1;
+  dfs(graph, 0, [0]);
+  function dfs(graph, node, path) {
+    if (node === target) {
+      result.push([...path]);
+      return;
+    }
+    for (const neighbor of graph[node]) {
+      path.push(neighbor);
+      dfs(graph, neighbor, path);
+      path.pop();
+    }
+  }
+  return result;
 }
